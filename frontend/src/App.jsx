2069780464f1e318c4158369d821e9ecef577a2b
Ramin293 <erikgan3@gmail.com>
2026-06-07 20:22:02 +0500

    frontend: add controled search form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,14 @@ import SearchResults from "./components/SearchResults"
 import Library from "./components/Library"
 
 function App() {
+  function handleSearch(query) {
+    console.log("Search query:", query)
+  }
+
   return (
     <main className="app">
       <Header />
-      <SearchBar />
+      <SearchBar onSearch={handleSearch}/>
       <SearchResults />
       <Library />
     </main>
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,9 +1,29 @@
-function SearchBar() {
+import { useState } from "react"
+
+function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState("")
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    if (!query.trim()) return
+
+    onSearch(query.trim())
+    setQuery("")
+  }
+
   return (
     <section className="panel">
       <h2 className="subtitle">Search Books</h2>
-      <form action="" className="search-form">
-        <input type="text" placeholder="Search by title, author or topic..."/>
+
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Search by title, author or topic..."
+          value = {query}
+          onChange = {(event) => setQuery(event.target.value)}
+          />
+
         <button type="submit">Search</button>
       </form>
     </section>
